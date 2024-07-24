a2228b49e29947b797f2c2dac87980a2a991c561
Abdallah <fafafavd@gmail.com>
2024-07-24 08:39:22 +0300

    Added GSAP animation

--- Frontend/src/components/RecipeList/index.jsx
+++ Frontend/src/components/RecipeList/index.jsx
@@ -3,18 +3,25 @@ import { Link } from "react-router-dom";
 import "./style.css"
 
 const RecipeList = ({ recipes }) => {
+
+  
+  useEffect(() => {
+    gsap.to(".recipe-item", {
+      duration: 1,
+      delay:0.5,
+      margin: 5,
+      ease: "power3.out",
+    });
+  }, [recipes]);
+
+
   return (
     <>
       {recipes.map((recipe) => (
         <div className="recipe-item">
           <Link to={"recipe/"+recipe.id_recipe} className="nav-link"><h2>{recipe.name}</h2></Link>
           <p className="description">{recipe.description}</p>
-          {/* <div className="steps">
-            <strong>Steps:</strong>
-            <ul>
-              {recipe.steps}
-            </ul>
-          </div> */}
+          
           <div className="ingredients">
             <strong>Ingredients:</strong>
             <ul>
@@ -26,14 +33,6 @@ const RecipeList = ({ recipes }) => {
         </div>
       ))}
     </>
-
-    // <div>
-    //     {recipes.map(recipe => (
-    //         <div key={recipe.id}>
-    //             <h2><Link to={`/recipe/${recipe.id}`}>{recipe.name}</Link></h2>
-    //         </div>
-    //     ))}
-    // </div>
   );
 };
 
